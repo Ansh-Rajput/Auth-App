e70453fcb4373c36f764c1a3caecba616d0e52eb
Ansh-Rajput <anshravikrish425317@gmail.com>
2024-02-01 13:35:20 +0530

    added custom register functionality without oAuth : with backEnd intigration

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/dp";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
 import { z } from "zod";
 
 export const register = async (values:z.infer<typeof RegisterSchema>) =>{
@@ -9,6 +12,21 @@ export const register = async (values:z.infer<typeof RegisterSchema>) =>{
     if(!validatedFields.success){
         return {error:'Invalid fields'};
     }
+    
+    const {email,password,name} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password,10);
 
-    return {success:"Loged In"};
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser){
+        return {error : "Email already in use!"};
+    }
+
+    await db.user.create({
+        data:{name,email,password:hashedPassword}
+    });
+
+    //TODO: send verification token;
+
+    return {success:"User Created"};
 }
